769e724d5ee8fadd7daccaf66a714dca6a77f814
pylemon <leeway1985@gmail.com>
2014-07-10 14:12:40 +0800

    change post method to get

--- validator/views.py
+++ validator/views.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.views.decorators.csrf import csrf_exempt
 from validator.utils import format_data, format_error
 
 
-@csrf_exempt
 def validate_menu_entry_data(request):
     """
 
@@ -13,15 +11,12 @@ def validate_menu_entry_data(request):
     :return:
     """
     context = RequestContext(request)
-
-    if request.method == 'POST':
-        data = request.POST.get('data')
-        formatted_data, error = format_data(data)
-        errors = format_error(error)
-        context.update({
-            'data': data,
-            'formatted_data': formatted_data,
-            'errors': errors,
-        })
-
+    data = request.GET.get('data', '')
+    formatted_data, error = format_data(data)
+    errors = format_error(error)
+    context.update({
+        'data': data,
+        'formatted_data': formatted_data,
+        'errors': errors,
+    })
     return render_to_response('validator/validate_menu_entry_data.html', context)
