37448d73395b5d3e8a53e177b84c7643ad7c9f5d
pylemon <leeway1985@gmail.com>
2014-07-10 12:10:09 +0800

    Update uwsgi conf

--- menuentry/wsgi.py
+++ menuentry/wsgi.py
@@ -6,8 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+
+_DIRNAME = os.path.dirname(globals()["__file__"])
+sys.path.append( os.path.join(_DIRNAME, '..') )
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "menuentry.settings")
 
 from django.core.wsgi import get_wsgi_application
