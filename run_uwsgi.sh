7c8fb23d0db7a8cd4afd41e2449ecab173a03f6d
pylemon <leeway1985@gmail.com>
2014-07-16 17:34:06 +0800

    add static map to uwsgi start script.

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 kill -9 `cat /tmp/menuentry.pid`
-/home/ubuntu/Envs/menuentry/bin/uwsgi --ini ./menuentry/uwsgi.ini
+/home/ubuntu/Envs/menuentry/bin/uwsgi --ini ./menuentry/uwsgi.ini --static-map /static=/home/ubuntu/menuentry/static/
